d19a46a5a61af1c87dfad055ec7164152a7d14d4
Christian Ramelow <info@codekandis.net>
2020-02-01 14:28:35 +0100

    Adds the URI builder configuration.

---
+++ config/uriBuilder.php
@@ -0,0 +1,9 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\EasyPwGenApi\Configurations;
+
+return [
+	'schema'       => 'https',
+	'host'         => 'api.easypwgen.codekandis',
+	'baseUri'      => '/',
+	'relativeUris' => []
+];
