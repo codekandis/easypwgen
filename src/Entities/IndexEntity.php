db1dd8a3e977fda9a7b5d144ed0c05bac718e051
Christian Ramelow <info@codekandis.net>
2020-02-01 14:34:03 +0100

    Adds the `IndexEntity`.

---
+++ src/Entities/IndexEntity.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\EasyPwGenApi\Entities;
+
+use CodeKandis\Tiphy\Entities\AbstractEntity;
+
+/**
+ * Represents the entity of the index.
+ * @package codekandis/easypwgen-api
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class IndexEntity extends AbstractEntity
+{
+}
