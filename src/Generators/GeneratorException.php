41d29b9675e6ef2a08a7d6608b08793e1ea0627f
Christian Ramelow <info@codekandis.net>
2020-02-01 12:49:02 +0100

    Adds the `GeneratorException`.

---
+++ src/Generators/GeneratorException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\EasyPwGenApi\Generators;
+
+use RuntimeException;
+
+/**
+ * Represents an exception of a generator.
+ * @package codekandis/easypwgen-api
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class GeneratorException extends RuntimeException
+{
+}
