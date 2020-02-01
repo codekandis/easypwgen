3753f829eebce5ed8b624979aa31dd856d6e09d7
Christian Ramelow <info@codekandis.net>
2020-02-01 15:22:39 +0100

    Adds the route to the `GetIndexAction` to the routes configuration.

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,11 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\EasyPwGenApi\Configurations;
 
+use CodeKandis\EasyPwGenApi\Actions\Api;
+use CodeKandis\Tiphy\Http\Requests\Methods;
+
 return [
+	'^/$' => [
+		Methods::GET => Api\Read\GetIndexAction::class
+	]
 ];
