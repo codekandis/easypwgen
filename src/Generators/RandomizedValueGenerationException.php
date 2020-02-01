9305fc9312e7418bb7e9253a5e79c7236086e16b
Christian Ramelow <info@codekandis.net>
2020-02-01 12:50:22 +0100

    Adds the `RandomizedValueGenerationException`.

---
+++ src/Generators/RandomizedValueGenerationException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\EasyPwGenApi\Generators;
+
+/**
+ * Represents an exception occurred during a randomized value generation.
+ * @package codekandis/easypwgen-api
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class RandomizedValueGenerationException extends GeneratorException
+{
+}
