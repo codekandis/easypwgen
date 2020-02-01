cfa428388011f653b2f52caba1279f7492faa924
Christian Ramelow <info@codekandis.net>
2020-02-01 14:36:10 +0100

    Adds the `ApiUriBuilder`.

---
+++ src/Http/UriBuilders/ApiUriBuilder.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\EasyPwGenApi\Http\UriBuilders;
+
+use CodeKandis\Tiphy\Http\UriBuilders\AbstractUriBuilder;
+
+/**
+ * Represents the URI builder of the API.
+ * @package codekandis/easypwgen-api
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class ApiUriBuilder extends AbstractUriBuilder
+{
+}
