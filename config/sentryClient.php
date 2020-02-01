6f201317ee9eaf183ff4126188690ec363a5ddfa
Christian Ramelow <info@codekandis.net>
2020-02-01 14:27:35 +0100

    Adds the `SentryClient` configuration.

---
+++ config/sentryClient.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\EasyPwGenApi\Configurations;
+
+return [
+	'dsn'           => '',
+	'displayErrors' => false,
+	'errorTypes'    => E_ALL,
+	'environment'   => 'development',
+	'release'       => 'dev-development',
+	'serverName'    => 'api.easypwgen.codekandis'
+];
