d2cee3b28365c5dbc765a16e9212be4f261a0a5c
Christian Ramelow <info@codekandis.net>
2021-01-31 22:10:23 +0100

    Splits the configuration `sentryClient`.

--- config/sentryClient.php
+++ config/sentryClient.php
@@ -1,11 +1,12 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\EasyPwGenApi\Configurations;
 
+use const E_ALL;
+
 return [
 	'dsn'           => '',
 	'displayErrors' => false,
 	'errorTypes'    => E_ALL,
 	'environment'   => 'development',
-	'release'       => 'dev-development',
 	'serverName'    => 'api.easypwgen.codekandis'
 ];
